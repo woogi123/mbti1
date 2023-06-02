e42ff4de3f908e31af29e0fefea2c1d48b106f7f
woogi123 <07491@naver.com>
2023-06-02 17:21:45 +0900

    mith

---
+++ index.js
@@ -0,0 +1,61 @@
+var num = "1";
+
+function start(){
+    $("#main").hide();
+    $("#qna").show();
+    next();
+}
+
+function next(){
+    //버튼 클릭 시 점수 계산
+    $("#A").click(function(){
+        var type = $("#type").val();
+        var preValue = $("#" + type).val();
+        $("#" + type).val(parseInt(preValue) + 1);
+        next();
+    });
+    $("#B").click(function(){
+        next();
+    });
+
+    if(num==13){
+        $("#qna").hide();
+        $("#result").show();
+
+        var mbti = "";
+        $("#EI").val() < 2 ? (mbti += "I") : (mbti += "E");
+        $("#SN").val() < 2 ? (mbti += "N") : (mbti += "S");
+        $("#TF").val() < 2 ? (mbti += "F") : (mbti += "T");
+        $("#JP").val() < 2 ? (mbti += "P") : (mbti += "J");
+
+        console.log(mbti);
+        $(".subtitle").html(result[mbti]["subtitle"]);
+        $(".explain").html(result[mbti]["explain"]);
+        $("#result_img").attr("src", result[mbti]["img"]);
+    } else{
+        $(".progress-bar").attr("style", "width:calc(100 / 12 *" + num + "%");
+        $("#title").html(q[num]["title"]);
+        $("#type").val(q[num]["type"]);
+        $("#A").html(q[num]["A"]);
+        $("#B").html(q[num]["B"]);
+        num++;
+    }
+}
+
+
+
+function shareURL(){
+    const dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+
+    const url = window.document.location.href;
+    dummy.value=url;
+
+    navigator.clipboard.writeText(dummy.value).then(() => {
+        alert("복사가 완료되었습니다.");
+    }).catch((err) => {
+        console.log('err:', err);
+    });
+
+    document.body.removeChild(dummy);
+}
